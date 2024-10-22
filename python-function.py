4152156a17cc226822bdd2c8735aaca2d5290aa3
rip-on <shipon.ripon123@gmail.com>
2024-10-22 22:04:14 +0600

    Python Function

---
+++ python-function.py
@@ -0,0 +1,9 @@
+print("Start of Program")
+def greetings(a):
+    print(a+" is attending ML course")
+    print("The course is online")
+
+greetings("Jahid")
+greetings("Jimmy")
+print("End of Program")
+
